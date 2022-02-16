8b9da602d12068cb2f21745f24badc5ee47428a4
Sameer Ahmed <sameer.ssj786@gmail.com>
2022-02-16 23:24:15 +0500

    added taliwind css

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup>
 import Header from "./components/Header.vue";
+import "./App.css";
 </script>
 
 <template>
   <Header />
-  <h1>Hello World</h1>
+  <h1 class="text-2xl text-teal-500 font-bold font-serif">Today Weather</h1>
 </template>
 
 <style>
